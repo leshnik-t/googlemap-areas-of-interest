2d364caa9188eb2f544c1f2ac031fa1e7a47c3de
leshnik-t <lenatl@gmail.com>
2024-01-06 10:27:41 +0200

    loading google map and drawing manager

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,86 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.scss';
+import { useRef, useEffect } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const mapDOMElement = useRef<HTMLElement>(null);
+  const mapRef = useRef<google.maps.Map | null>(null);
+  const drawingRef = useRef<google.maps.drawing.DrawingManager | null>(null);
+  
+  useEffect(() => {
+      const initMap = async (): Promise<void> => {
+      // The location of Uluru
+        const initPosition = { lat: 25.774, lng: -80.19 };
+        // Request needed libraries.
+        try {
+          const { Map } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
+          const { DrawingManager } = await google.maps.importLibrary("drawing") as google.maps.DrawingLibrary;
+          // const { AdvancedMarkerElement } = await google.maps.importLibrary("marker") as google.maps.MarkerLibrary; 
+          
+          if(mapDOMElement.current) {
+            const googleMapObject = new Map(
+              mapDOMElement.current,
+              {
+                zoom: 5,
+                center: initPosition,
+                mapId: 'AOI_MAP_ID',
+                mapTypeId: 'hybrid',
+                disableDefaultUI: true,
+                zoomControl: true,
+                scaleControl: true,
+                gestureHandling: "greedy",
+                maxZoom: 17,
+                minZoom: 4
+              }
+            );
+            mapRef.current = googleMapObject;
+           
+            /// Drawing Manager
+            const svgMarker = {
+              path: "M64 32C64 14.3 49.7 0 32 0S0 14.3 0 32V64 368 480c0 17.7 14.3 32 32 32s32-14.3 32-32V352l64.3-16.1c41.1-10.3 84.6-5.5 122.5 13.4c44.2 22.1 95.5 24.8 141.7 7.4l34.7-13c12.5-4.7 20.8-16.6 20.8-30V66.1c0-23-24.2-38-44.8-27.7l-9.6 4.8c-46.3 23.2-100.8 23.2-147.1 0c-35.1-17.6-75.4-22-113.5-12.5L64 48V32z",
+              fillColor: "blue",
+              fillOpacity: 0.6,
+              strokeWeight: 0,
+              rotation: 0,
+              scale: 0.05,
+              anchor: new google.maps.Point(0, 20),
+            };
+
+            const drawingManagerObject = new DrawingManager({
+              drawingMode: null,
+              drawingControl: true,
+              drawingControlOptions: {
+                position: google.maps.ControlPosition.RIGHT_BOTTOM,
+                drawingModes: [
+                  google.maps.drawing.OverlayType.MARKER,
+                  google.maps.drawing.OverlayType.CIRCLE,
+                  google.maps.drawing.OverlayType.POLYGON,
+                  google.maps.drawing.OverlayType.POLYLINE,
+                  google.maps.drawing.OverlayType.RECTANGLE,
+                ],
+              },
+              markerOptions: {
+                icon: svgMarker,
+              },
+              polygonOptions: {
+              },
+            });
+            drawingRef.current = drawingManagerObject;
+          } 
+        } catch(e) {
+          console.log(e);
+        }
+      }
+      initMap();
+      
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <section id="map" ref={mapDOMElement}>
+          
+      </section>
     </>
-  )
+  );
 }
 
 export default App
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// export default ({ mode }) => {
+//   // Load app-level env vars to node-level env vars.
+//   // process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+//   // process.env.VITE_GOOGLE_API_KEY
+//   return defineConfig({
+//     plugins: [react()],
+    
+//     // To access env vars here use process.env.TEST_VAR
+//   });
+// }
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
